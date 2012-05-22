1435c8a00171abb758562a5de759cd882525f06b
zyl.skysniper <zyl.skysniper@gmail.com>
2012-05-22 06:07:20 +0000

    加入清屏功能

---
+++ client/Android/v1/src/com/me/whiteboard/actions/ClearAction.java
@@ -0,0 +1,41 @@
+package com.me.whiteboard.actions;
+
+import android.graphics.Canvas;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.me.whiteboard.Display;
+import com.me.whiteboard.MainActivity;
+
+public class ClearAction extends Action {
+
+	public ClearAction() {
+	}
+
+	public ClearAction(short type, short usr_ID, short local_ID) {
+		super(type, usr_ID, local_ID);
+	}
+
+	@Override
+	public void act(MainActivity activity, Canvas canvas) {
+		Display.minDisplayTime = System.currentTimeMillis();
+		activity.rePaint();
+	}
+
+	@Override
+	public byte[] privateToBytes() {
+		return null;
+	}
+
+	@Override
+	protected void bytesToPrivate(byte[] bytes) {
+	}
+
+	@Override
+	public View getView(LayoutInflater mLayoutInflater, int mResource,
+			View convertView, ViewGroup parent, boolean selected) {
+		return null;
+	}
+
+}
