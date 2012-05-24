2d9d68c50370783cef1aeb8c7feedb9014de8187
zyl.skysniper@gmail.com <zyl.skysniper@gmail.com@5bf7d64b-5ff2-82f2-c8e4-0f0b0850b2ee>
2012-05-24 03:32:41 +0000

    撤销OK了

---
+++ trunk/client/Android/v1/src/com/me/whiteboard/actions/TextAction.java
@@ -0,0 +1,42 @@
+package com.me.whiteboard.actions;
+
+import android.graphics.Canvas;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.me.whiteboard.MainActivity;
+
+public class TextAction extends Action {
+	
+	private String text;
+
+	public TextAction() {
+		super();
+		type = TYPE_TEXT;
+	}
+
+	public TextAction(short usr_ID, short local_ID) {
+		super(TYPE_TEXT, usr_ID, local_ID);
+	}
+
+	@Override
+	public void act(MainActivity activity, Canvas canvas) {
+	}
+
+	@Override
+	public byte[] privateToBytes() {
+		return null;
+	}
+
+	@Override
+	protected void bytesToPrivate(byte[] bytes) {
+	}
+
+	@Override
+	public View getView(LayoutInflater mLayoutInflater, int mResource,
+			View convertView, ViewGroup parent, boolean selected) {
+		return null;
+	}
+
+}
