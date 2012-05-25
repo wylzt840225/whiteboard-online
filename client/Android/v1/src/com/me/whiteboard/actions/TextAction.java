f163a0dbfa4626da36aa5af6ce3ba2c3cd3fec86
zyl.skysniper <zyl.skysniper@gmail.com>
2012-05-25 04:56:22 +0000

    textAction

--- client/Android/v1/src/com/me/whiteboard/actions/TextAction.java
+++ client/Android/v1/src/com/me/whiteboard/actions/TextAction.java
@@ -1,14 +1,21 @@
 package com.me.whiteboard.actions;
 
+import org.apache.http.util.ByteArrayBuffer;
+
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.me.whiteboard.Display;
 import com.me.whiteboard.MainActivity;
 
 public class TextAction extends Action {
 
+	private int color;
+	private float textSize;
+	private float x, y;
 	private String text;
 
 	public TextAction() {
@@ -22,15 +29,44 @@ public class TextAction extends Action {
 
 	@Override
 	public void act(MainActivity activity, Canvas canvas) {
+		Paint paint = new Paint(MainActivity.paint);
+		paint.setColor(color);
+		paint.setTextSize(Display.width_AbsoluteToScreen(textSize));
+		canvas.drawText(text, x, y, paint);
 	}
 
 	@Override
 	public byte[] privateToBytes() {
-		return null;
+		ByteArrayBuffer baf = new ByteArrayBuffer(10 + text.getBytes().length);
+		
+		byte[] bytes = intToBytes(color);
+		baf.append(bytes, 0, bytes.length);
+		
+		bytes = shortToBytes((short) (textSize / Display.screen_width * Short.MAX_VALUE));
+		baf.append(bytes, 0, bytes.length);
+		
+		bytes = shortToBytes((short) (x / Display.screen_width * Short.MAX_VALUE));
+		baf.append(bytes, 0, bytes.length);
+		
+		bytes = shortToBytes((short) (y / Display.screen_width * Short.MAX_VALUE));
+		baf.append(bytes, 0, bytes.length);
+		
+		bytes = text.getBytes();
+		baf.append(bytes, 0, bytes.length);
+		
+		return baf.toByteArray();
 	}
 
 	@Override
 	protected void bytesToPrivate(byte[] bytes) {
+		color = bytesToInt(bytes[0], bytes[1], bytes[2], bytes[3]);
+		textSize = (float) bytesToShort(bytes[4], bytes[5])
+				/ Short.MAX_VALUE * Display.screen_width;
+		x = (float) bytesToShort(bytes[6], bytes[7])
+				/ Short.MAX_VALUE * Display.screen_width;
+		y = (float) bytesToShort(bytes[8], bytes[9])
+				/ Short.MAX_VALUE * Display.screen_width;
+		text = new String(bytes, 10, bytes.length - 10);
 	}
 
 	@Override
